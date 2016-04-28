09f919e6d1e7d685dcb4b4225992c2218ad501d0
suporte <suporte@DESKTOP-I992IHS.redeirdeb.intranet>
2016-04-28 16:31:39 -0300

    adicionado campos alunos

--- presencaUfba/src/br/com/dados/Aluno.java
+++ presencaUfba/src/br/com/dados/Aluno.java
@@ -1,5 +1,51 @@
 package br.com.dados;
 
+import java.util.List;
+
 public class Aluno {
+	private int id;
+	private String matricula;
+	private String nome;
+	private List<Boolean> presenca;
+	
+	public Aluno(int id, String matricula,String nome, List<Boolean> presenca) {
+		// TODO Auto-generated constructor stub
+		setId(id);
+		setMatricula(matricula);
+		setNome(nome);
+		setPresenca(presenca);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Boolean> getPresenca() {
+		return presenca;
+	}
+
+	public void setPresenca(List<Boolean> presenca) {
+		this.presenca = presenca;
+	}
 
 }
